43ace9ce6c90ce2e84031885b7452b9e7a58f95c
MikSuki <34766025+MikSuki@users.noreply.github.com>
2019-04-11 06:24:56 +0800

    change beads from darwImg to imgElement

--- js/draw.js
+++ js/draw.js
@@ -26,7 +26,7 @@ function drawStartView() {
         sCtx.globalAlpha = 0.025 * t;
 
         sCtx.drawImage(imgDic['startBack'], x, y, w, h);
-    }, 5.0);
+    }, 50);
 
 }
 
@@ -39,11 +39,11 @@ function breakStartView() {
             clearInterval(s);
             return;
         }
-        startCanvas.style.opacity  = 1 - 0.025 * t;
-    }, 7.5);
+        startCanvas.style.opacity = 1 - 0.025 * t;
+    }, 75);
 }
 
-function drawGameView(){
+function drawGameView() {
     var t = 0;
     audioDic["start"].pause();
     var s = setInterval(() => {
@@ -51,27 +51,22 @@ function drawGameView(){
             sCtx.globalAlpha = 1;
             sCtx.fillRect(0, 0, canvasWidth, canvasHeight);
             setTimeout(() => {
-                mainCanvas.style.opacity = 1;
-                mainCanvas.style.opacity = 1;
-                if(music) audioDic["bgm"].play();
+                beadsGroup.style.opacity = 1;
+                backCanvas.style.opacity = 1;
+                if (music) audioDic["bgm"].play();
                 gameStart = true;
                 canPlay = true;
             }, 500);
             clearInterval(s);
             return;
         }
-        mainCanvas.style.opacity  = 0.05 * t;
-        backCanvas.style.opacity  = 0.05 * t;
+        beadsGroup.style.opacity = 0.05 * t;
+        backCanvas.style.opacity = 0.05 * t;
     }, 60);
 }
 
-
-
-
-
-
-
 function drawGrid() {
+
     bCtx.drawImage(imgDic['grid'], startX, startY, gridSize * 4, gridSize * 4);
     bCtx.drawImage(imgDic['grid'], 0, 0, 168, 168, startX + gridSize * 4, startY, gridSize * 2, gridSize * 2);
     bCtx.drawImage(imgDic['grid'], 0, 0, 168, 168, startX + gridSize * 4, startY + gridSize * 2, gridSize * 2, gridSize * 2);
@@ -80,7 +75,6 @@ function drawGrid() {
     bCtx.drawImage(imgDic['grid'], 0, 0, 168, 84, startX + gridSize * 4, startY + gridSize * 4, gridSize * 2, gridSize);
     drawBar(0, 1);
 }
-
 
 function drawBar(w, mode) {
     var w = (!w) ? gridSize * 6 : w;
@@ -104,46 +98,20 @@ function clearBar() {
     bCtx.clearRect(startX, startY - h * 1.75, h * 2, h);
 }
 
-
-// beads
-function drawBeads() {
-    for (var i = 0; i < 6; ++i) {
-        var img = imgDic['bead' + i.toString()];
-        for (var j = 0; j < 30; ++j) {
-            if (j === hover)
-                continue;
-            if (beads[j].type === i) {
-                mCtx.drawImage(img, beads[j].x, beads[j].y, gridSize, gridSize);
-            }
-        }
-    }
-    if (isClick) {
-        var img = imgDic['bead' + (clickedBead.type).toString()];
-        mCtx.globalAlpha = 0.3;
-        mCtx.drawImage(img, beads[hover].x, beads[hover].y, gridSize, gridSize);
-        mCtx.globalAlpha = 1;
-    }
+function drawTransparentBead() {
+    transparentBead = createBead(hover, beads[hover].type)
+    transparentBead.style.opacity = 0.3
 }
 
 function drawDropBeads() {
     var i, j, k;
     for (i = 0; i < 6; ++i) {
-        var len = dropBeads[i].length;
-        var x = beads[i].x;
-        for (j = 0; j < len; ++j) {
-            if (!dropBeads[i][j].dropGrid) continue;
-            mCtx.clearRect(x, dropBeads[i][j].y - sub, gridSize, gridSize);
-        }
-    }
-    for (i = 0; i < 6; ++i) {
-        var img = imgDic['bead' + i.toString()];
         for (j = 0; j < 6; ++j) {
             var len = dropBeads[j].length;
-            var x = beads[j].x;
             for (k = 0; k < len; ++k) {
                 if ((dropBeads[j][k].type !== i) || (dropBeads[j][k].dropGrid <= 0)) continue;
                 dropBeads[j][k].y += sub;
-                mCtx.drawImage(img, x, dropBeads[j][k].y, gridSize, gridSize);
+                dropBeads[j][k].img.style.top = dropBeads[j][k].y + 'px'
                 --dropBeads[j][k].dropGrid;
             }
         }
@@ -153,63 +121,90 @@ function drawDropBeads() {
 function drawNewBeads() {
     var i, j, k;
     for (i = 0; i < 6; ++i) {
-        var len = newBeads[i].length;
-        var x = beads[i].x;
-        for (j = 0; j < len; ++j) {
-            if (!newBeads[i][j].dropGrid) continue;
-            mCtx.clearRect(x, newBeads[i][j].y - sub, gridSize, gridSize);
-        }
-    }
-
-    for (i = 0; i < 6; ++i) {
-        var img = imgDic['bead' + i.toString()];
         for (j = 0; j < 6; ++j) {
             var len = newBeads[j].length;
-            var x = beads[j].x;
             for (k = 0; k < len; ++k) {
                 if ((newBeads[j][k].type !== i) || (newBeads[j][k].dropGrid <= 0)) continue;
                 newBeads[j][k].y += sub;
-                mCtx.drawImage(img, x, newBeads[j][k].y, gridSize, gridSize);
+                newBeads[j][k].img.style.top = newBeads[j][k].y + 'px'
                 --newBeads[j][k].dropGrid;
             }
         }
     }
 }
 
-
-// redraw when move beads
 function drawMove() {
-    var x = clickedBead.x;
-    var y = clickedBead.y;
-    // clear only a part 
-    mCtx.clearRect(x - gridSize * 2, y - gridSize * 2, gridSize * 4, gridSize * 4);
-    mCtx.clearRect(startX, startY, endX, endY);
-    drawBeads();
-    mCtx.globalAlpha = 0.5;
-    var img = imgDic['bead' + (clickedBead.type).toString()];
-    mCtx.drawImage(img, x, y, gridSize, gridSize);
-    mCtx.globalAlpha = 1;
-    mCtx.drawImage(imgDic['cursor'], x, y);
+    transparentBead.style.left = beads[hover].x + 'px'
+    transparentBead.style.top = beads[hover].y + 'px'
 }
 
-// redraw when input up or out 
 function drawUp() {
-    var x = beads[hover].x;
-    var y = beads[hover].y;
-    // clear only a part 
-    mCtx.clearRect(x - gridSize * 2, y - gridSize * 2, gridSize * 4, gridSize * 4);
-    mCtx.clearRect(startX, startY, endX, endY);
-    drawBeads();
-    var img = imgDic['bead' + (beads[hover].type).toString()];
-    mCtx.drawImage(img, x, y, gridSize, gridSize);
+    beads[hover].img.style.left = beads[hover].x + 'px'
+    beads[hover].img.style.top = beads[hover].y + 'px'
+    transparentBead.style.display = 'none'
 }
 
-// redraw when clear beads
 function drawClear(thisClear) {
-    for (k = 0; k < thisClear.length; ++k) {
-        var index = thisClear[k];
-        var x = startX + (index % 6) * gridSize;
-        var y = startY + Math.floor(index / 6) * gridSize;
-        mCtx.clearRect(x, y, gridSize, gridSize);
-    }
+    thisClear.forEach(e => {
+        beads[e].img.style.display = 'none'
+    });
+}
+
+
+function drawBoom(thisClear) {
+    const divide = 3
+    thisClear.forEach(e => {
+        var imgDiv = document.createElement('div')
+        var imgList = []
+        var unit = ~~(gridSize / divide)
+
+        for (var i = 0; i < divide * divide; ++i) {
+            var img = new Image()
+            img.src = selBeadsType(beads[e].type)
+            img.style.width = gridSize + 'px'
+            img.style.height = gridSize + 'px'
+            img.style.position = 'absolute'
+            img.style.left = beads[e].x + 'px'
+            img.style.top = beads[e].y + 'px'
+            img.style.zIndex = '1000'
+            img.style.clip = 'rect(' + unit * (~~(i / divide)) + 'px ' + unit * (i % divide + 1) + 'px ' + unit * (~~(i / divide + 1)) + 'px ' + unit * (i % divide) + 'px)'
+            img.style.transform = 'scale(' + Math.random() * 1.5 + ')'
+            img.style.transform = 'rotate(' + Math.random() * 360 + 'deg)'
+
+            imgList.push(img)
+            imgDiv.appendChild(img)
+        }
+
+        beadsGroup.appendChild(imgDiv)
+
+
+        var t = 0
+        var s = setInterval(() => {
+            var i = 0
+            if (++t > 25) {
+                clearInterval(s)
+                imgDiv.parentNode.removeChild(imgDiv)
+                return
+            }
+            imgList.forEach(e => {
+                var x = parseInt(e.style.left)
+                var y = parseInt(e.style.top)
+
+                x += (i % divide < 2) ? Math.random() * -2 : Math.random() * 2
+                y += (i / divide < 2) ? Math.random() * -2 : Math.random() * 2
+                ++i
+                e.style.left = x + 'px'
+                e.style.top = y + 'px'
+            });
+        }, boomT)
+    });
+}
+
+function drawMiku() {
+    imgDic["miku"].style.position = 'absolute'
+    imgDic["miku"].style.width = ~~(gridSize * 3.5) + 'px'
+    imgDic["miku"].style.height = ~~(gridSize * 3.5 * 1049 / 1007) + 'px'
+    imgDic["miku"].style.left = ~~(startX + gridSize * 3 - gridSize * 3.5 / 2) + 'px'
+    imgDic["miku"].style.height = ~~(gridSize * 3.5 * 1049 / 1007) + 'px'
+    beadsGroup.appendChild(imgDic["miku"])
 }
